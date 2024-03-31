c7dc82f4d75b2b405c11dd486b8f180157286596
yuanda@yume <34027873+yuandahanif@users.noreply.github.com>
2024-03-31 12:47:09 +0700

    pass all test

--- api/user/request.go
+++ api/user/request.go
@@ -7,6 +7,6 @@ type RegisterPayload struct {
 }
 
 type LoginPayload struct {
-	Email    string `json:"email" binding:"required"`
-	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=5,max=15"`
 }
